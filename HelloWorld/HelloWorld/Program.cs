d290252ec0bd3b3e79d99c4076b3edf63437f227
shaynaBiohacked <shayna.jamieson@biohacked.com>
2020-03-08 19:15:43 -0700

    Add initial project

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; // name space in .NET framework
+
+namespace HelloWorld 
+{
+    class MainClass
+    {
+        static void Main(string[] args) // entry point of the application
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
