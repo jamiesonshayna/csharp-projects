a46ea9c205612d32a4b0fd3465aaab213e6b140e
jamiesonshayna <sjamieson2@mail.greenriver.edu>
2020-03-09 20:35:54 -0700

    Structs v Classes

---
+++ Non-Primitive-Types/Non-Primitive-Types/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NonPrimitiveTypes
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi! My name is " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ Non-Primitive-Types/Non-Primitive-Types/Program.cs
@@ -0,0 +1,26 @@
+﻿using NonPrimitiveTypes.Math;
+
+namespace NonPrimitiveTypes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Person object
+            Person me = new Person();
+            me.FirstName = "Shayna";
+            me.LastName = "Jamieson";
+            me.Introduce();
+
+            // Calculator object
+            Calculator calc = new Calculator();
+            System.Console.WriteLine(calc.Add(44, 7));
+        }
+    }
+}
+
+/* struct v. class
+ *
+ *  use a structure when you want to define
+ *  a small and light-weight object
+ */
