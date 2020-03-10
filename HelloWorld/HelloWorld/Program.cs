6edcc1a57e2c008701bb99e175d0d99151638d2a
jamiesonshayna <sjamieson2@mail.greenriver.edu>
2020-03-09 20:09:31 -0700

    Primitive types lecture

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,11 +4,44 @@ namespace HelloWorld
 {
     class MainClass
     {
-        static void Main(string[] args) // entry point of the application
+        static void Main(string[] args)
         {
             Console.BackgroundColor = ConsoleColor.DarkRed;
 
-            Console.WriteLine("Hello World!");
+            // naming conventions
+            // local vars -> camelCase
+            // constant vars -> PascalCase
+
+            // variables and constants
+            byte number = 100;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string name = "Shayna";
+            bool isWorking = true;
+
+            const float Pi = 2.14f;
+
+            // keyboard shortcut for console.write -> cw + tab
+            Console.WriteLine(number);
+            Console.WriteLine(count + totalPrice);
+            Console.WriteLine(character + " name is " + name);
+            Console.WriteLine(isWorking);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+
+            // implicit type conversion (won't compile if data loss is percieved)
+            byte b = 1;
+            int i = b;
+
+            // force the type conversion (explicit type conversion (cast))
+            int l = 1;
+            byte b2 = (byte) l;
+
+            // non-compatible types won't compile (unless converted)
+            string s = "1";
+            int i2 = Convert.ToInt32(s); // conversions with Convert class
+            int j = int.Parse(s); // primitives all have parse method
         }
     }
 }
